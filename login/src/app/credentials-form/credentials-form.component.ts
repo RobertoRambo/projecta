3ab7619511d46ec53a9b390fdb064d8c2b462c98
Tobias Heise <theise@mila.com>
2019-08-27 19:50:32 +0200

    Refactored login form, implemented password strength functionality

--- login/src/app/credentials-form/credentials-form.component.ts
+++ login/src/app/credentials-form/credentials-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnSubmit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import * as zxcvbn from 'zxcvbn';
+import { FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-credentials-form',
@@ -8,19 +9,51 @@ import * as zxcvbn from 'zxcvbn';
 })
 export class CredentialsFormComponent implements OnInit {
 
-  constructor() {
-  	this.credentials = new Credentials();
-  }
+  private passwordStrengthScore: number;
+  private passwordStrength: string;
+  credentialsForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.credentialsForm = this.formBuilder.group({
+      email: '',
+      password: ''
+    });
+    this.onChanges();
   }
 
-  ngOnChanges(changes) {
-  	console.log(changes);
+  onChanges(): void {
+    this.credentialsForm.get('password').valueChanges.subscribe(val => {
+      if (val) {
+        this.updateStrength(val);
+      } else {
+        this.passwordStrengthScore = 0;
+      }
+    });
   }
 
-  onSubmit(credentialsForm) { 
-  	console.log(credentialsForm.value); 
+  onSubmit(credentialsForm) {
+    console.log(credentialsForm.value);
+    this.credentialsForm.reset();
+    this.passwordStrength = '';
+  }
+
+  updateStrength(val) {
+    this.passwordStrengthScore = zxcvbn(val).score;
+    switch (this.passwordStrengthScore) {
+      case 2:
+        this.passwordStrength = 'weak';
+        break;
+      case 3:
+        this.passwordStrength = 'middle';
+        break;
+      case 4:
+        this.passwordStrength = 'strong';
+        break;
+      default:
+        this.passwordStrength = 'invalid';
+    }
   }
 
 }
